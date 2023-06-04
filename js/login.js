ef5248ed508fda4a3f48deed399fcfd5b7106e9a
노주희 <sarah19270@naver.com>
2023-06-04 18:20:56 +0000

    자바 10주차 응용문제

--- js/login.js
+++ js/login.js
@@ -1,17 +1,84 @@
+
+//시작할 때 아이디 기억
+function init(){
+	let id = document.querySelector("#floatingInput");
+	let check = document.querySelector("#idSaveCheck");
+	let get_id = getCookie("id");
+	
+	if(get_id){
+		id.value = get_id;
+		check.checked = true;
+	}
+}
+
 function login(){
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
-    
+    let check = document.querySelector("#idSaveCheck");
+	
     form.action = "../index_login.html";
     form.method = "get"
-    
-    if(id.value.length === 0 || password.value.length === 0){
+	
+    if(check.checked == true){
+		alert("쿠키를 저장합니다.");
+		setCookie("id",id.value,1);
+		alert("쿠키값 : " + id.value);
+	}
+	else { 
+		setCookie("id",id.value,0);
+	}
+	
+	login_count();
+	
+	let rst = login_check();
+	console.log(rst);
+	
+	
+	
+	if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.")
     }
+	else if(rst == true){
+		form.submit();
+	}
 	else{
-        form.submit();
+        return;
     }
+	
+	
+}
+
+function setCookie(name, value, expiredays) {
+        var date = new Date();		// 시간 불러오는 함수
+        date.setDate(date.getDate() + expiredays);	//set 날짜함수는 날짜 데이터의 원하는 형식만 다른 값으로 교체하는 함수
+        // 현재 날짜에 원하는만료일수를 더함
+		document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite=None; Secure";;        
+    }
+
+// 쿠키를 Get 하는 함수 : 쿠키를 얻음
+function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {	// 쿠키가 존재하면
+            var cookie_array = cookie.split("; ");
+			//console.log(cookie_array);
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                //console.log(cookie_name);
+                if (cookie_name[0] == "id") {	//키가 팝업YN이면
+                    return cookie_name[1];		//값을 리턴
+                }
+            }
+        }
+        return ;
+}
+
+//만료기한 지나면 쿠키 삭제하는것 같음
+function deleteCookie(cookieName){
+	var expireDate = new Date();
+	expireDate.setDate(expireDate.getDate() - 1);
+	document.cookie = cookieName + "= " + "; expires="+ expireDate.toGMTString();
 }
 
 function logout(){
@@ -45,17 +112,85 @@ function get_id(){
 //정규 표현식 : 문자열에서 특정 내용을 찾거나 대체 또는 발췌하는데 사용
 // 이메일과 비밀번호 형태를 제대로 띄고 있나 검사하는 용도 같음.
 function login_check(){
-	var url = location.href;
-	var imformation = (url.slice(url.indexOf("?")+1, url.length)).split("&");
-	var email = decodeURIComponent(imformation[0].split('=')[1]);
-	var pw = imformation[1].split('=')[1];
+	//var url = location.href;
+	//var imformation = (url.slice(url.indexOf("?")+1, url.length)).split("&");
+	//var email = decodeURIComponent(imformation[0].split('=')[1]);
+	//var pw = imformation[1];
+	
+	let id = document.querySelector("#floatingInput");
+    let password = document.querySelector("#floatingPassword");
+	
 	var regexr1 = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 	//메일 주소 형식에 맞는지 검사한다.
 	var regexr2 = /^[A-Za-z0-9]{4,10}$/;
 	// 알파벳 대소문자 또는 숫자로 시작하고 끝나며 4 ~10자리인지 검사
 	// {4,10}: 4 ~ 10자리
-	console.log(regexr1.test(email));
-	console.log(regexr2.test(pw));
+	
+	 
+	let rst1 = regexr1.test(id.value);
+	let rst2 = regexr2.test(password.value);
+	
+	if(rst1 != true){
+		alert("이메일 형식을 확인해주세요.");
+	}
+	
+	else if(rst2 != true){
+		alert("비밀번호 형식이 알파벳 대소문자 또는 숫자로 시작하고 끝나며 4 ~10자리인지 확인해주세요.");
+	}
+	
+	else{
+		return true;
+	}
+		//return(rst1,rst2);
+	//return regexr1.test(email), regexr2.test(pw);
+}
+
+
+//10주차 응용문제 
+function login_count(){
+	var get_logincookie = getloginCookie("login_cnt");
+	
+	if(get_logincookie == ""){
+		let cnt = Number(0);
+	}
+	else{
+		cnt = get_logincookie;
+		setloginCookie("login_cnt",cnt) ;
+		console.log(get_logincookie);
+	}
+	
+	if (get_logincookie > 5){
+		alert("로그인가능 횟수를 초과했습니다.")
+	}
+	
 	
 }
 
+function setloginCookie(name, value) {
+        // 현재 날짜에 원하는만료일수를 더함
+		document.cookie = escape(name) + "=" + escape(value) ;        
+    }
+
+// 쿠키를 Get 하는 함수 : 쿠키를 얻음
+function getloginCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {	// 쿠키가 존재하면
+			var cookie_array = cookie.split("; ");
+			console.log(cookie_array);
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                console.log(cookie_name);
+                if (cookie_name[0] == "login_cnt") {	//키가 팝업YN이면
+					
+					let cookiename = Number(cookie_name[1]);
+					cookiename += 1;
+					//console.log(typeof(cookiename));
+                    return cookiename;		//값을 리턴
+					
+                }
+            }
+            
+        }
+        return ;
+}
